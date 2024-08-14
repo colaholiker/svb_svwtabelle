375b0420da43462da4900dba25466544acd5c0fd
Sebastian During <colaholiker@nixos1.svbalingen.de>
2024-08-14 13:19:16 +0200

    asdf

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -7,7 +7,7 @@ call_user_func(
 	{
 
 		\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-			'SchachvereinBalingen.SvbSvwtabelle',
+			'SvbSvwtabelle',
 			'Viewtabelle',
 			'viewtabelle'
 		);
